74860aa46644db4d2d7b6b6a2c31285c716cb491
Jim Narey <jimnarey@me.com>
2022-06-29 02:07:45 +0100

    Add more archive types

--- extract.go
+++ extract.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"path"
+
 	"github.com/gen2brain/go-unarr"
+	"github.com/mholt/archiver"
 )
+
+const fixturesPath string = "./fixtures"
 
 type archive interface {
 	extractAllTo(string) error
 }
 
 type unarrArchive struct {
-	//archive unarr.Archive
 	path string
 }
 
@@ -27,6 +33,18 @@ func (u unarrArchive) extractAllTo(path string) error {
 	return nil
 }
 
+type archiverArchive struct {
+	path string
+}
+
+func (a archiverArchive) extractAllTo(path string) error {
+	err := archiver.Unarchive(a.path, path)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func newUnarrArchive(path string) (archive, error) {
 	a, err := unarr.NewArchive(path)
 	if err != nil {
@@ -36,20 +54,49 @@ func newUnarrArchive(path string) (archive, error) {
 	return unarrArchive{path: path}, nil
 }
 
+func newArchiverArchive(path string) (archive, error) {
+	fmt.Println()
+	a, err := archiver.ByExtension(path)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("a: %v\n", a)
+	if err != nil {
+		return nil, err
+	}
+	return archiverArchive{path: path}, nil
+}
+
+func getArchive(path string, archiveGetters []func(string) (archive, error)) (archive, error) {
+	for i := 0; i < len(archiveGetters); i++ {
+		archive, err := archiveGetters[i](path)
+		if err == nil {
+			fmt.Printf("%T", archive)
+			return archive, nil
+		}
+		fmt.Println(err)
+	}
+	return nil, errors.New("No compatible unarchiver found")
+}
 
 func main() {
 
-	//archives:= []string{"./fixtures/file.txt.zip", "./fixtures/file.txt.7z", "./fixtures/file.txt.rar", "./fixtures/file.txt.tar", "./fixtures/file.txt.gz"}
-	archives:= []string{"./fixtures/file.txt.zip", "./fixtures/file.txt.7z", "./fixtures/file.txt.tar", "./fixtures/file.txt.gz"}
+	archiveGetters := []func(string) (archive, error){newArchiverArchive, newUnarrArchive}
 
-	for i:= 0; i < len(archives); i++ {
-		a, err := newUnarrArchive(archives[i])
+	archives, err := ioutil.ReadDir(fixturesPath)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(archives); i++ {
+		fmt.Println("**************")
 		fmt.Println(archives[i])
+		a, err := getArchive(path.Join(fixturesPath, archives[i].Name()), archiveGetters)
 		if err != nil {
 			fmt.Println(err)
 
 		} else {
-			fmt.Println(a)
+			fmt.Printf("%T\n", a)
 		}
 	}
 
